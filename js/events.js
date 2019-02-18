c67d91b6687c510b66cea2a5def4116bc36ccdcc
Mike Doig <mike.j.doig@gmail.com>
2019-02-18 20:02:49 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -13,7 +13,7 @@ function submitIt() {
 }
 function pressIt() {
  $('#typing').on('keydown', function (e){
-   if (e.which == 103) {
+   if (e.which == 71) {
    alert('You pressed the G key');
 }
 });
